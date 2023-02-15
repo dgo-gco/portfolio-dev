8815dae8b61a29d47fbd9d37dc97469cd0b58071
Diego <110729317+dgo-gco@users.noreply.github.com>
2023-02-15 22:41:56 +0100

    :sparkles: giving style to the projects

--- src/projects-data.ts
+++ src/projects-data.ts
@@ -1,10 +1,11 @@
 export const projects = [
   {
     id: "1",
-    link: "https://brilliant-praline-302aac.netlify.app",
-    img: "../images/JavaScript-interactive-site.png",
-    name: "E-commerce JS Project",
-    description: "Une fois que j'ai acquis les bases de JS, j'ai commencé à manipuler le DOM d'une manière très stimulante, en essayant de ne pas chercher de solutions sur Internet, mais plutôt en cherchant par moi-même le plus possible.",
+    link: "https://incandescent-douhua-84d310.netlify.app/",
+    img: "../images/Self-care-center.png",
+    name: "Self Care Center",
+    description:
+      "C’est une application qui aide les utilisateurs à se rappeler de leur valeur intrinsèque en affichant une affirmation ou un mantra aléatoire.",
   },
   {
     id: "2",
@@ -15,11 +16,11 @@ export const projects = [
   },
   {
     id: "3",
-    link: "https://incandescent-douhua-84d310.netlify.app/",
-    img: "../images/Self-care-center.png",
-    name: "Self Care Center",
+    link: "https://simplon-site-html-css-js.netlify.app/",
+    img: "../images/Simplon-site-formation.png",
+    name: "Simplon.co website clone",
     description:
-      "C’est une application qui aide les utilisateurs à se rappeler de leur valeur intrinsèque en affichant une affirmation ou un mantra aléatoire.",
+      "En suivant un fichier Figma fourni, j'ai créé un clone du site Simplon. C'était un projet difficile en raison du délai qui nous était imparti. Mais je suis très satisfait du résultat.",
   },
   {
     id: "4",
@@ -31,10 +32,9 @@ export const projects = [
   },
   {
     id: "5",
-    link: "https://simplon-site-html-css-js.netlify.app/",
-    img: "../images/Simplon-site-formation.png",
-    name: "Simplon.co website clone",
-    description:
-      "En suivant un fichier Figma fourni, j'ai créé un clone du site Simplon. C'était un projet difficile en raison du délai qui nous était imparti. Mais je suis très satisfait du résultat.",
+    link: "https://brilliant-praline-302aac.netlify.app",
+    img: "../images/JavaScript-interactive-site.png",
+    name: "E-commerce JS Project",
+    description: "Une fois que j'ai acquis les bases de JS, j'ai commencé à manipuler le DOM d'une manière très stimulante, en essayant de ne pas chercher de solutions sur Internet, mais plutôt en cherchant par moi-même le plus possible.",
   }
 ];
